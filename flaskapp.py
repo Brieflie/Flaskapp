4a9bc99efd2beb09961c397c87c6dda78bfeb7b8
Brieflie <63971543+Brieflie@users.noreply.github.com>
2021-04-23 11:24:40 +0200

    Push template and static folder

---
+++ flaskapp.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from mysql_acces import *
+
+app = Flask(__name__)
+app.secret_key = "hello"
+app.config['SQLALCHEMY_DATABASE_URI'] = mysql_key
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    email = db.Column("email", db.String(100), primary_key=True)
+    password = db.Column("password", db.String(100))
+
+
+    def __init__(self, password, email):
+        self.password = password
+        self.email = email
+
+@app.route("/", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["email"]
+        password = request.form["password"]
+        session["user"] = user
+
+        found_user = users.query.filter_by(email=user, password=password).first()
+        if found_user:
+            session["email"] = found_user.email
+            return redirect(url_for("index"))
+        else:
+            flash("Wrong user or password")
+            return render_template("login.html")
+    else:
+        if "user" in session:
+            return redirect(url_for("index"))
+        else:
+            return render_template("login.html")
+
+@app.route("/signup", methods=["POST","GET"])
+def signup():
+    if "user" in session:
+        return redirect(url_for("index"))
+    else:
+        return render_template("signup.html")
+
+@app.route("/index")
+def index():
+    try:
+        user = session["user"]
+        return render_template('index.html', user=user)
+    except KeyError:
+        return redirect(url_for("login"))
+
+@app.route("/widgets")
+def widgets():
+    try:
+        user = session["user"]
+        return render_template('widgets.html', user=user)
+    except KeyError:
+        return redirect(url_for("login"))
+
+@app.route("/table-data")
+def table_data():
+    try:
+        user = session["user"]
+        return render_template('table-data.html', user=user)
+    except KeyError:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        flash("You have been logged out!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    #db.create_all()
+    app.run(debug=True)
---
+++ mysql_acces.py
@@ -0,0 +1,1 @@
+mysql_key = 'mysql://brieflie_db:P=K(+[h]ow[%@108.167.189.58/brieflie_users'
